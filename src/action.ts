cdff5e3cf7adc279fbe4953ca59e3cfd9ced4683
Alex Miller <codex.nz@gmail.com>
2021-07-26 16:15:34 +1200

    Add Action typings and config loading.

---
+++ src/action.ts
@@ -0,0 +1,79 @@
+import * as core from "@actions/core";
+
+const WORKFLOW_TIMEOUT_SECONDS = 60;
+
+/**
+ * action.yaml definition.
+ */
+export interface ActionConfig {
+  /**
+   * GitHub API token for making requests.
+   */
+  token: string;
+
+  /**
+   * The git reference for the workflow. The reference can be a branch or tag name.
+   */
+  ref: string;
+
+  /**
+   * Repository of the action to await.
+   */
+  repo: string;
+
+  /**
+   * Owner of the given repository.
+   */
+  owner: string;
+
+  /**
+   * Workflow to return an ID for. Can be the ID or the workflow filename.
+   */
+  workflow: string | number;
+
+  /**
+   * Time until giving up waiting for the start of the workflow run.
+   */
+  workflowTimeoutSeconds?: number;
+}
+
+export function getConfig(): ActionConfig {
+  return {
+    token: core.getInput("token", { required: true }),
+    ref: core.getInput("ref", { required: true }),
+    repo: core.getInput("repo", { required: true }),
+    owner: core.getInput("owner", { required: true }),
+    workflow: getWorkflowValue(core.getInput("workflow", { required: true })),
+    workflowTimeoutSeconds:
+      getNumberFromValue(core.getInput("workflowTimeoutSeconds")) ||
+      WORKFLOW_TIMEOUT_SECONDS,
+  };
+}
+
+function getNumberFromValue(value: string): number | undefined {
+  if (value === "") {
+    return undefined;
+  }
+
+  try {
+    const num = parseInt(value);
+
+    if (isNaN(num)) {
+      throw new Error("Parsed value is NaN");
+    }
+
+    return num;
+  } catch (error) {
+    throw new Error(`Unable to parse value: ${value}`);
+  }
+}
+
+function getWorkflowValue(workflowInput: string): string | number {
+  try {
+    // We can assume that the string is defined and not empty at this point.
+    return getNumberFromValue(workflowInput)!;
+  } catch {
+    // Assume using a workflow name instead of an ID.
+    return workflowInput;
+  }
+}
---
+++ tests/action.test.ts
@@ -0,0 +1,70 @@
+import * as core from "@actions/core";
+import { ActionConfig, getConfig } from "../src/action";
+
+describe("Action", () => {
+  describe("getConfig", () => {
+    const mockEnvConfig = {
+      token: "secret",
+      ref: "feature_branch",
+      repo: "repository",
+      owner: "owner",
+      workflow: "workflow_name",
+      workflowTimeoutSeconds: "60",
+    };
+
+    beforeEach(() => {
+      jest.spyOn(core, "getInput").mockImplementation((input: string) => {
+        switch (input) {
+          case "token":
+            return mockEnvConfig.token;
+          case "ref":
+            return mockEnvConfig.ref;
+          case "repo":
+            return mockEnvConfig.repo;
+          case "owner":
+            return mockEnvConfig.owner;
+          case "workflow":
+            return mockEnvConfig.workflow;
+          case "workflowTimeoutSeconds":
+            return mockEnvConfig.workflowTimeoutSeconds;
+          default:
+            throw new Error("invalid input requested");
+        }
+      });
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it("should return a valid config", () => {
+      const config: ActionConfig = getConfig();
+
+      // Assert that the numbers / types have been properly loaded.
+      expect(config.token).toStrictEqual("secret");
+      expect(config.ref).toStrictEqual("feature_branch");
+      expect(config.repo).toStrictEqual("repository");
+      expect(config.owner).toStrictEqual("owner");
+      expect(config.workflow).toStrictEqual("workflow_name");
+      expect(config.workflowTimeoutSeconds).toStrictEqual(60);
+    });
+
+    it("should have a number for a workflow when given a workflow ID", () => {
+      const originalWorkflow = mockEnvConfig.workflow;
+      mockEnvConfig.workflow = "123456";
+      const config: ActionConfig = getConfig();
+      mockEnvConfig.workflow = originalWorkflow;
+
+      expect(config.workflow).toStrictEqual(123456);
+    });
+
+    it("should provide a default workflow timeout if none is supplied", () => {
+      const originalWorkflowTimeout = mockEnvConfig.workflowTimeoutSeconds;
+      mockEnvConfig.workflowTimeoutSeconds = "";
+      const config: ActionConfig = getConfig();
+      mockEnvConfig.workflowTimeoutSeconds = originalWorkflowTimeout;
+
+      expect(config.workflowTimeoutSeconds).toStrictEqual(60);
+    });
+  });
+});
