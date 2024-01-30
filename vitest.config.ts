613608c4dc13fe9e5a6a782affd10634bca81c07
Alex Miller <codex.nz@gmail.com>
2024-01-30 14:17:29 +1300

    refactor: use vitest cfg instead of vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     coverage: {
       provider: "v8",
       reporter: ["text", "lcov"],
+      include: ["src/**/*.ts"],
     },
   },
 });
