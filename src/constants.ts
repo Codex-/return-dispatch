2d9b3d0843aa9b68215616dd0bef13e25a39d521
Alex Miller <codex.nz@gmail.com>
2024-10-05 12:26:11 +1300

    refactor: use number instead of inferred constant value

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
-export const WORKFLOW_FETCH_TIMEOUT_MS = 60 * 1000;
-export const WORKFLOW_JOB_STEPS_RETRY_MS = 5000;
-export const WORKFLOW_JOB_STEPS_SERVER_ERROR_RETRY_MAX = 3;
-export const WORKFLOW_JOB_STEPS_SERVER_ERROR_RETRY_MS = 500;
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+
+export const WORKFLOW_FETCH_TIMEOUT_MS: number = 60 * 1000;
+export const WORKFLOW_JOB_STEPS_RETRY_MS: number = 5000;
+export const WORKFLOW_JOB_STEPS_SERVER_ERROR_RETRY_MAX: number = 3;
+export const WORKFLOW_JOB_STEPS_SERVER_ERROR_RETRY_MS: number = 500;
